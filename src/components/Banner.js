4f1cdd632dc144cef0bf03980cf7067945dd43fc
Panji Gumelar <panjiggm@gmail.com>
2019-01-09 13:54:42 +0700

    refactoring styled component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,20 +1,22 @@
 import React, { Component } from "react";
-import { Segment, Message, Card } from "semantic-ui-react";
+import { Segment, Message, Divider } from "semantic-ui-react";
 
 class Banner extends Component {
   render() {
     return (
       <Segment color="red">
         {this.props.calegInfo.map((caleg, index) => {
+          console.log(caleg.dapil);
           return (
             <Message color="olive" key={index}>
-              <div className="header">{caleg.nama}</div>
-              <p className="description">Dapil : {caleg.dapil}</p>
-              <ul className="list">
-                <li>{caleg.program1}</li>
-                <li>{caleg.program2}</li>
-                <li>{caleg.program3}</li>
-              </ul>
+              <Message.Header>{caleg.nama}</Message.Header>
+              <Divider />
+              <div>Dapil : {caleg.dapil}</div>
+              <Message.List>
+                <Message.Item>{caleg.program1}</Message.Item>
+                <Message.Item>{caleg.program2}</Message.Item>
+                <Message.Item>{caleg.program3}</Message.Item>
+              </Message.List>
             </Message>
           );
         })}
